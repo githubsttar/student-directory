5b7a33975d66e720e8c5d7610cb06bac21a57fdb
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2018-10-24 19:10:14 +0100

    control flow added

--- directory.rb
+++ directory.rb
@@ -4,20 +4,20 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
-  origin = gets.chomp
-  height = gets.chomp
-  cohort = gets.chomp
+  name = gets.chop
+  origin = gets.chop
+  height = gets.chop
+  cohort = gets.chop
   # while the name is not empty, repeat this code
   while !name.empty?do
     # add the student hash to the array
     students << {name: name, cohort: cohort, origin: origin, height: height}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
-    origin = gets.chomp
-    height = gets.chomp
-    cohort = gets.chomp
+    name = gets.chop
+    origin = gets.chop
+    height = gets.chop
+    cohort = gets.chop
   end
   # return the array of students
   students
@@ -55,7 +55,9 @@ end
 
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count >= 1
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
 students = input_students
